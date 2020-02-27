45246e2056252ea4f20f1b397e10b8f7ceebbf16
shgzb99 <lanhaitao@gmail.com>
2020-02-27 21:33:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_generate
+sed -i '1i src-git luci https://github.com/rosywrt/luci.git' feeds.conf.default
