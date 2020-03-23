49ddaafe91472c7d1c9d951469075245de500752
shgzb99 <lanhaitao@gmail.com>
2020-03-23 19:26:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_generate
 #sed -i '1i src-git luci https://github.com/rosywrt/luci.git' feeds.conf.default
+
+cd openwrt/package
+git clone https://github.com/tty228/luci-app-serverchan.git
+https://github.com/destan19/OpenAppFilter.git
